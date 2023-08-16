3e75d2ce49c4224cb28785504926a0dbbbd418ed
Qamar <qazam@lucidya.com>
2023-08-16 16:26:16 +0300

    fix app name

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,30 +1,32 @@
-
 import { Link } from 'react-router-dom';
 import { useAuthState } from '../contexts/UserContext';
 import { SignInButton } from '../domain/auth/SignInButton';
 import { SignOutButton } from '../domain/auth/SignOutButton';
 import { SignUpButton } from '../domain/auth/SignUpButton';
-import { useLocation } from "react-router-dom"
-
-
+import { useLocation } from 'react-router-dom';
 
 function Header() {
-    const userAuth =   useAuthState();
-    const user = userAuth?.user;
-    let location = useLocation();
+  const userAuth = useAuthState();
+  const user = userAuth?.user;
+  let location = useLocation();
 
-    return (
-        <>
-            <nav className="p-4 mb-20 flex items-center justify-between">
-                <Link to="/" className="text-slate-100 text-xl text-bold">Tool App</Link>
+  return (
+    <>
+      <nav className='p-4 mb-20 flex items-center justify-between'>
+        <Link to='/' className='text-slate-100 text-xl text-bold'>
+          Todo App
+        </Link>
 
-                {user ? <SignOutButton /> : location.pathname === "/signin" ? <SignUpButton /> : <SignInButton />}
-
-
-
-            </nav>
-        </>
-    )
+        {user ? (
+          <SignOutButton />
+        ) : location.pathname === '/signin' ? (
+          <SignUpButton />
+        ) : (
+          <SignInButton />
+        )}
+      </nav>
+    </>
+  );
 }
 
 export default Header;
